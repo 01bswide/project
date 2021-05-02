eb91c408be51fa7dab726de97b322b803cd72a92
01bswide <ben.swiderski@outlook.com>
2021-05-02 14:43:34 +0100

    added urql provider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,20 @@
-import { createClient, Provider } from 'urql';
+import { createClient, Provider } from "urql";
 
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
 
 const client = createClient({
-  url: 'http://localhost:4000/graphql',
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
 
 function MyApp({ Component, pageProps }: any) {
-  return (<Provider value={client}><Component {...pageProps} /></Provider>)
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
